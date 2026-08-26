7e17196951cd6668b08724620c7463ce3b4fb238
Stephan Hüwe <45753488+sHuewe@users.noreply.github.com>
2026-08-26 18:57:14 +0200

    Export script repository API

--- signaldeck_sdk/__init__.py
+++ signaldeck_sdk/__init__.py
@@ -2,6 +2,8 @@ from .context import ApplicationContext
 from .value_provider import ValueProvider
 from .cmd import Cmd, Command, EchoCommand, SleepCommand
 from .cmdResult import CmdResult
+from .script import ScriptDefinition, ScriptVariable
+from .script_repository import ScriptRepository
 from .processor.processor import Processor, Placeholder
 from .processor.display_data import DisplayData
 from .processor.display_processor import DisplayProcessor
@@ -9,4 +11,4 @@ from .persistence.data_store import DataStore
 from .persistence.field import Field
 from .message import Message, MessageBus, MessageListener
 from .persistence.persist_data import PersistData
-__all__ = ["ApplicationContext","CmdResult", "Cmd", "Command", "EchoCommand", "SleepCommand", "CmdResult", "Processor", "Placeholder", "DisplayData", "DisplayProcessor", "DataStore", "Field", "PersistData", "ValueProvider","Message","MessageBus","MessageListener",]
+__all__ = ["ApplicationContext", "CmdResult", "Cmd", "Command", "EchoCommand", "SleepCommand", "ScriptDefinition", "ScriptVariable", "ScriptRepository", "Processor", "Placeholder", "DisplayData", "DisplayProcessor", "DataStore", "Field", "PersistData", "ValueProvider", "Message", "MessageBus", "MessageListener"]
