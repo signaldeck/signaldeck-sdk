ccfe34b69bd8c497828a278c12b63a366da7c7fb
Stephan Hüwe <45753488+sHuewe@users.noreply.github.com>
2026-08-27 13:24:38 +0200

    Export script control flow APIs

--- signaldeck_sdk/__init__.py
+++ signaldeck_sdk/__init__.py
@@ -1,9 +1,13 @@
 from .context import ApplicationContext
 from .value_provider import ValueProvider
 from .cmd import Cmd, Command, EchoCommand, SleepCommand
+from .condition_command import ConditionCommand
 from .cmdResult import CmdResult
+from .execution_context import ExecutionContext
 from .script import ScriptDefinition, ScriptVariable
 from .script_repository import ScriptRepository
+from .script_parser import ScriptParser, ScriptSyntaxError
+from .script_statement import CommandStatement, SetStatement, IfStatement, WhileStatement
 from .alias import AliasDefinition
 from .alias_repository import AliasRepository
 from .processor.processor import Processor, Placeholder
@@ -15,8 +19,11 @@ from .message import Message, MessageBus, MessageListener
 from .persistence.persist_data import PersistData
 
 __all__ = [
-    "ApplicationContext", "CmdResult", "Cmd", "Command", "EchoCommand", "SleepCommand",
-    "ScriptDefinition", "ScriptVariable", "ScriptRepository", "AliasDefinition", "AliasRepository",
-    "Processor", "Placeholder", "DisplayData", "DisplayProcessor", "DataStore", "Field",
-    "PersistData", "ValueProvider", "Message", "MessageBus", "MessageListener"
+    "ApplicationContext", "CmdResult", "Cmd", "Command", "ConditionCommand",
+    "EchoCommand", "SleepCommand", "ExecutionContext", "ScriptDefinition",
+    "ScriptVariable", "ScriptRepository", "ScriptParser", "ScriptSyntaxError",
+    "CommandStatement", "SetStatement", "IfStatement", "WhileStatement",
+    "AliasDefinition", "AliasRepository", "Processor", "Placeholder", "DisplayData",
+    "DisplayProcessor", "DataStore", "Field", "PersistData", "ValueProvider",
+    "Message", "MessageBus", "MessageListener"
 ]
