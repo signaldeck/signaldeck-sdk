3bf666b18bdc1a018205e75b9bc38b89a3bd085d
Stephan Hüwe <45753488+sHuewe@users.noreply.github.com>
2026-08-27 08:20:12 +0200

    Store Cmd run variables in results

--- signaldeck_sdk/cmdResult.py
+++ signaldeck_sdk/cmdResult.py
@@ -1,16 +1,18 @@
 from datetime import datetime
 
-class CmdResult():
-    def __init__(self):
-        self._finished=False
-        self.state=[]
+
+class CmdResult:
+    def __init__(self, variables=None):
+        self._finished = False
+        self.state = []
+        self.variables = dict(variables or {})
 
     def finish(self):
-        self._finished=True
+        self._finished = True
 
     def isFinished(self):
         return self._finished
-    
-    def appendState(self,command,**kwargs):
-        data = {"command":command.name,"date":datetime.now(), **kwargs}
+
+    def appendState(self, command, **kwargs):
+        data = {"command": command.name, "date": datetime.now(), **kwargs}
         self.state.append(data)
